89bb621a4ec39ea39ab08a0001ef41ad2a7d7f94
Knut Ahlers <knut@ahlers.me>
2020-07-16 00:17:27 +0200

    Don't fail make run when not explicitly asked
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- scripts/has_archive_mix.sh
+++ scripts/has_archive_mix.sh
@@ -8,9 +8,9 @@ set -euo pipefail
 
 [[ $(find . -name '*.tar.xz' | wc -l) -gt 0 ]] && {
 	echo "Both XZ and zst archives found, pay attention!"
-}
+} || true
 
 [[ $(find . -name '*.db.tar.xz' | wc -l) -gt 0 ]] && [[ $(find . -name '*.db.tar.zst' | wc -l) -gt 0 ]] && {
 	echo "Found XZ and zst databases! Check this!"
 	exit 1
-}
+} || true
